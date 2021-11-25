a8fd3eb941b6ea7e230f7f61d8fbc9a2899e9562
Matthew Santiago <carag.matthew@gmail.com>
2021-11-25 00:10:33 -0600

    Loading database in runtime

--- Assets/Scripts/LearnedMotionMatching/Animator_MotionMatching.cs
+++ Assets/Scripts/LearnedMotionMatching/Animator_MotionMatching.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Animator_MotionMatching : MonoBehaviour
 {
-    private Animator _animator;
+    [SerializeField] private Avatar _avatar;
+    [SerializeField] private string _databaseFilePath = "Assets\\MatchingFeaturesDatabase.sloth";
     // [SerializeField] private GameObject _hips = null;
     // [SerializeField] private GameObject _leftFoot = null;
     // [SerializeField] private GameObject _rightFoot = null;
@@ -31,49 +31,7 @@ public class Animator_MotionMatching : MonoBehaviour
 
     private void Start()
     {
-        // TODO: We'll be using this to start playing on a certain spot of a sequence
-        _animator = this.GetComponent<Animator>();
-        _database = MatchingFeaturesDatabase.Instance;
-
-        //_animator.Play("Base Layer.Idle", 0, 0); // Resume from beginning
-    }
-
-    private void LateUpdate()
-    {
-        if (!_isLoaded)
-        {
-            // Load all feature vectors
-            _animator.applyRootMotion = true;
-            _animator.speed = 0;
-            AnimationClip ac = _animator.runtimeAnimatorController.animationClips[_currentClip];
-            int numFrames = (int)(ac.frameRate * ac.length);
-            if (_currentFrame < numFrames)
-            {
-                float t = (float)(_currentFrame / numFrames);
-                _animator.Play("Base Layer." + ac.name, 0, t);
-                SetFeatures(ac.frameRate);
-                float[] featureVector = GetCurrentFeatureVector();
-                _database.AddToDatabase(ac.name, featureVector);
-                _currentFrame++;
-            }
-            else
-            {
-                _currentClip++;
-                _currentFrame = 0;
-            }
-            _animator.applyRootMotion = false;
-            _animator.speed = 1;
-            if (_currentClip == _animator.runtimeAnimatorController.animationClips.Length - 1)
-            {
-                _database.Print();
-                _isLoaded = true;
-            }
-        }
-
-        // (string name, float normalizedTime) t = _database.GetClosestFeature(featureVector);
-        // _animator.Play(t.name, 0, t.normalizedTime);
-
-        // Debug.Log(s);
+        _database = new MatchingFeaturesDatabase(_databaseFilePath);
     }
 
     private void OnAnimatorIK(int layerIndex)
@@ -85,18 +43,18 @@ public class Animator_MotionMatching : MonoBehaviour
 
     private void SetFeatures(float dt)
     {
-        Vector3 newLeftFootPosition = _animator.GetIKPosition(AvatarIKGoal.LeftFoot);
-        Vector3 newRightFootPosition = _animator.GetIKPosition(AvatarIKGoal.RightFoot);
-        Vector3 newHipPosition = _animator.rootPosition;
+        // Vector3 newLeftFootPosition = _animator.GetIKPosition(AvatarIKGoal.LeftFoot);
+        // Vector3 newRightFootPosition = _animator.GetIKPosition(AvatarIKGoal.RightFoot);
+        // Vector3 newHipPosition = _animator.rootPosition;
 
         // Velocities are small.. we may have to do this for all the animations
-        _leftFootVelocity = (newLeftFootPosition - _prevLeftFootPos) / dt;
-        _rightFootVelocity = (newRightFootPosition - _prevRightFootPos) / dt;
-        _hipVelocity = (newHipPosition - _hipPosition) / dt;
+        // _leftFootVelocity = (newLeftFootPosition - _prevLeftFootPos) / dt;
+        // _rightFootVelocity = (newRightFootPosition - _prevRightFootPos) / dt;
+        // _hipVelocity = (newHipPosition - _hipPosition) / dt;
 
-        _prevLeftFootPos = newLeftFootPosition;
-        _prevRightFootPos = newRightFootPosition;
-        _hipPosition = newHipPosition;
+        // _prevLeftFootPos = newLeftFootPosition;
+        // _prevRightFootPos = newRightFootPosition;
+        // _hipPosition = newHipPosition;
 
         // TODO: Obtain trajectory positions
         // TODO: Obtain trajectory velocities
@@ -135,40 +93,30 @@ public class Animator_MotionMatching : MonoBehaviour
     private float[] GetCurrentFeatureVector()
     {
         float[] currentFeatureVector = new float[27];
-        // Calculate true local position - feet 
-        Vector3 leftFootPosition = _animator.GetIKPosition(AvatarIKGoal.LeftFoot);
-        Vector3 rightFootPosition = _animator.GetIKPosition(AvatarIKGoal.RightFoot);
 
         // Get feet position
-        currentFeatureVector[0] = leftFootPosition.x;
-        currentFeatureVector[1] = leftFootPosition.y;
-        currentFeatureVector[2] = leftFootPosition.z;
+        // currentFeatureVector[0] = leftFootPosition.x;
+        // currentFeatureVector[1] = leftFootPosition.y;
+        // currentFeatureVector[2] = leftFootPosition.z;
 
-        currentFeatureVector[3] = rightFootPosition.x;
-        currentFeatureVector[4] = rightFootPosition.y;
-        currentFeatureVector[5] = rightFootPosition.z;
+        // currentFeatureVector[3] = rightFootPosition.x;
+        // currentFeatureVector[4] = rightFootPosition.y;
+        // currentFeatureVector[5] = rightFootPosition.z;
 
-        currentFeatureVector[6] = _leftFootVelocity.x;
-        currentFeatureVector[7] = _leftFootVelocity.y;
-        currentFeatureVector[8] = _leftFootVelocity.z;
+        // currentFeatureVector[6] = _leftFootVelocity.x;
+        // currentFeatureVector[7] = _leftFootVelocity.y;
+        // currentFeatureVector[8] = _leftFootVelocity.z;
 
-        currentFeatureVector[9] = _rightFootVelocity.x;
-        currentFeatureVector[10] = _rightFootVelocity.y;
-        currentFeatureVector[11] = _rightFootVelocity.z;
+        // currentFeatureVector[9] = _rightFootVelocity.x;
+        // currentFeatureVector[10] = _rightFootVelocity.y;
+        // currentFeatureVector[11] = _rightFootVelocity.z;
 
-        currentFeatureVector[12] = _hipVelocity.x;
-        currentFeatureVector[13] = _hipVelocity.y;
-        currentFeatureVector[14] = _hipVelocity.z;
+        // currentFeatureVector[12] = _hipVelocity.x;
+        // currentFeatureVector[13] = _hipVelocity.y;
+        // currentFeatureVector[14] = _hipVelocity.z;
 
         // TODO: Obtain trajectory positions
-        currentFeatureVector[15] = _animator.velocity.x;
-        currentFeatureVector[16] = _animator.velocity.y;
-        currentFeatureVector[17] = _animator.velocity.z;
-
         // TODO: Obtain trajectory velocities
-        currentFeatureVector[21] = _animator.velocity.x;
-        currentFeatureVector[22] = _animator.velocity.y;
-        currentFeatureVector[23] = _animator.velocity.z;
         return currentFeatureVector;
     }
 }
--- Assets/Scripts/LearnedMotionMatching/MatchingFeaturesDatabase.cs
+++ Assets/Scripts/LearnedMotionMatching/MatchingFeaturesDatabase.cs
@@ -1,22 +1,26 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
 /// <summary> </summary>
-public class MatchingFeaturesDatabase : MonoBehaviour
+public class MatchingFeaturesDatabase
 {
-    private static MatchingFeaturesDatabase _instance;
-    public static MatchingFeaturesDatabase Instance
-    {
-        get { return _instance; }
-    }
+    // private static MatchingFeaturesDatabase _instance;
+    // public static MatchingFeaturesDatabase Instance
+    // {
+    //     get { return _instance; }
+    // }
+
+    private List<AnimationClip> _animationClips;
 
     private List<(string name, float[] featureVector)> _database;
 
-    [SerializeField] private string _databaseFile;
+    private string _databaseFilePath;
+    private string _databaseText;
 
     // TODO: Have C++ handle calls to Tensorflow models
     [DllImport("MotionMatchingUnityPlugin", CallingConvention = CallingConvention.Cdecl)]
@@ -31,22 +35,47 @@ public class MatchingFeaturesDatabase : MonoBehaviour
     [DllImport("MotionMatchingUnityPlugin", CallingConvention = CallingConvention.Cdecl)]
     private static extern float AddTwoFloats(float f1, float f2);
 
-    private void Awake()
-    {
-        // This class is intended for use as a Singleton
-        if (_instance != null && _instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            _instance = this;
-        }
-    }
+    // private void Awake()
+    // {
+    //     // This class is intended for use as a Singleton
+    //     if (_instance != null && _instance != this)
+    //     {
+    //         Destroy(this.gameObject);
+    //     }
+    //     else
+    //     {
+    //         _instance = this;
+    //     }
+    // }
 
-    private void Start()
+    public MatchingFeaturesDatabase(string databaseFilePath)
     {
         _database = new List<(string, float[])>();
+        _animationClips = new List<AnimationClip>();
+        _databaseFilePath = databaseFilePath;
+
+        if (!File.Exists(_databaseFilePath))
+        {
+            Debug.LogError("Unable to find database file: " + _databaseFilePath);
+        }
+
+        Debug.Log("Parsing database file: " + _databaseFilePath);
+        foreach (string line in File.ReadLines(_databaseFilePath))
+        {
+            string[] featureVectorStrings = line.Split(',');
+            string name = featureVectorStrings[0];
+            float[] featureVector = featureVectorStrings.Select(f =>
+            {
+                if (float.TryParse(f, out float number))
+                {
+                    return number;
+                }
+                return float.MinValue;
+            }).ToArray();
+            AddToDatabase(name, featureVector);
+        }
+
+        Print();
     }
 
     public void AddToDatabase(string name, float[] featureVector)
